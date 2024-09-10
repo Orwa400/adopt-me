b43fbecf7bd26fbd371bef780f74266443837a21
Orwa400 <matheworwa3@gmail.com>
2024-09-10 22:55:35 +0300

    Add components to App

---
+++ src/App.js
@@ -0,0 +1,37 @@
+const Pet = (props) => {
+    return React.createElement("div", {}, [
+        React.createElement("h1", {}, props.name),
+        React.createElement("h2", {}, props.animal),
+        React.createElement("h2", {}, props.breed),
+    ]);
+};
+
+const App = () => {
+    return React.createElement(
+      "div",
+      {},
+      [
+        React.createElement("h1", {}, "Adopt Me!"),
+        React.createElement(Pet, {
+            animal: "Dog",
+            name: "Khalifa",
+            breed: "Havanese"
+        }),
+        React.createElement(Pet, {
+            animal: "Cat",
+            name: "Casper",
+            breed: "mixed"
+        }),
+        React.createElement(Pet, {
+            animal: "Bird",
+            name: "Wunna",
+            breed: "Parrot"
+        }),
+      ]
+    )
+  };
+  
+  const container = document.getElementById("root");
+  const root = ReactDOM.createRoot(container);
+  root.render(React.createElement(App));
+             
